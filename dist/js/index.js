3cdba60619584806b0cb02d44ed9a7e624cd368c
mopsfl <floda854@gmail.com>
2023-07-11 14:47:27 +0200

    added files via git source control

---
+++ dist/js/index.js
@@ -0,0 +1,5 @@
+// index.ts
+var home_landingpage = document.querySelector("#home_landingpage");
+window.onload = async () => {
+  home_landingpage;
+};
---
+++ index.ts
@@ -0,0 +1,5 @@
+const home_landingpage = document.querySelector("#home_landingpage")
+
+window.onload = async () => {
+    home_landingpage
+}
